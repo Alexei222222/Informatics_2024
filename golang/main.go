628433a53b3ba66bb6a7e777a2263cc6d3f9a4fc
Alexei222222 <aleksmadaev@gmail.com>
2024-11-04 16:51:52 +0300

    lab6

--- golang/main.go
+++ golang/main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 
 	"isuct.ru/informatics2022/lab4"
+	"isuct.ru/informatics2022/lab6"
 )
 
 func main() {
 	fmt.Println("Мадаев Алексей Андреевич")
-
+	lab6.RunLab6Tasks()
 	lab4.RunLab4()
 }
